362d3a42de7ab52111ecf27a06bec675b729d975
Manuel Pablo Ramos Aguilar <93121628+pramagu@users.noreply.github.com>
2023-05-03 09:36:41 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-    apiUser: 'https://microservicioauth.heroku.app.com',
-  apiHistorial: 'https://microserviciohistorial.heroku.app.com',
-  apiOperaciones: 'https://microserviciooperacion.heroku.app.com',
+    apiUser: 'https://microservicioauth.herokuapp.com',
+  apiHistorial: 'https://microserviciohistorial.herokuapp.com',
+  apiOperaciones: 'https://microserviciooperacion.herokuapp.com',
 };
