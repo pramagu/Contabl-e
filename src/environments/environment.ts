b6798977437a1c7da93fd9bf4b7edc093dc74ee5
Manuel Pablo Ramos Aguilar <93121628+pramagu@users.noreply.github.com>
2023-05-10 10:15:59 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   apiUser: 'https://microservicioauth.herokuapp.com',
   apiHistorial: 'https://microserviciohistorial.herokuapp.com',
   apiOperaciones: 'https://microserviciooperacion.herokuapp.com',
+  apiNotificaciones: 'https://microservicionotificacion.herokuapp.com',
 };
 
 /*
